866ee4dab799c29dec40ba3870b34ef87934289b
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-06-15 16:22:07 -0400

    feat: Add artist tiles.

--- app/routes/artists.js
+++ app/routes/artists.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from "@ember/service";
 
 export default class ArtistsRoute extends Route {
-  @service store;
-
   model() {
-    return this.store.findAll("artist");
+    return this.store.findAll("artist", { include: "team,country" });
   }
 }
