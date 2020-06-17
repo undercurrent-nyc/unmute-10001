f74e6dd6e54aec7e7722a27ec5bff5ceb60feda4
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-06-16 23:25:19 -0400

    feat: Build out team page.

--- app/routes/teams/team.js
+++ app/routes/teams/team.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class TeamsTeamRoute extends Route {
+  model(params) {
+    return this.store.findRecord("team", params.team_id, { include: "artists,artists.country,clips" });
+  }
 }
