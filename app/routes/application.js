b1eca6b2a41c0b7a124b511a34943e5476f63c61
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-06-19 20:41:58 -0400

    feat: add small header svg

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,4 @@
 import Route from '@ember/routing/route';
-import { inject as service } from "@ember/service";
 
 export default class ApplicationRoute extends Route {
-
-  @service intl;
-
-  beforeModel() {
-    this._super(...arguments);
-    this.intl.setLocale(['en-us']);
-  }
 }
