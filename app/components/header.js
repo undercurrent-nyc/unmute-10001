53e56b8b382e7a437ceebc2a1b58deb7e7c0f017
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-06-20 17:30:58 -0400

    feat: animate small header

--- app/components/header.js
+++ app/components/header.js
@@ -1,20 +1,25 @@
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
+import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
 
 export default class HeaderComponent extends Component {
+  @action
+  *transition({ insertedSprites, removedSprites }) {
+    insertedSprites.forEach(fadeIn);
+    removedSprites.forEach(fadeOut);
+  }
+
   @tracked
   showSmallHeader = false;
 
   @action
   onEnter(){
     this.showSmallHeader = false;
-    console.log("entered");
   }
 
   @action
   onExit(){
     this.showSmallHeader = true;
-    console.log("exited");
   }
 }
