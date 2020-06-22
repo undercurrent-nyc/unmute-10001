28bc829184177cb869b7e69bb26d29fb6c1573e6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-06-22 19:08:06 -0400

    feat: hide large header on splash page

--- app/components/header.js
+++ app/components/header.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
 
 export default class HeaderComponent extends Component {
+  @service router;
+
   @action
   *transition({ insertedSprites, removedSprites }) {
     insertedSprites.forEach(fadeIn);
@@ -21,5 +24,13 @@ export default class HeaderComponent extends Component {
   @action
   onExit(){
     this.showSmallHeader = true;
+    return true;
+  }
+
+  constructor() {
+    super(...arguments);
+    if(this.router.currentRouteName === "splash_page") {
+      this.showSmallHeader = true;
+    }
   }
 }
