5583b6948e89eeb4746d53909ccd06234eb76576
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-07-13 00:55:15 -0400

    feat: Make format-date() more normal.

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,8 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function formatDate(date) {
-  const dateTimeFormat = new Intl.DateTimeFormat("en", { month: "numeric", day: "2-digit" });
-  const [{ value: month },,{ value: day }] = dateTimeFormat.formatToParts(Date.parse(date)) ;
-  return `${month}/${day}`;
+  return date.toLocaleString("en-US", {
+    month: "numeric",
+    day: "numeric",
+  });
 });
