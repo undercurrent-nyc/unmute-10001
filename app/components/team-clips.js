8910c1e83a499c02bf4e6ebb8fd306858f53fb5b
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-07-16 16:43:02 -0400

    feat: Add randomness into team clips throughout

---
+++ app/components/team-clips.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+
+export default class TeamClipsComponent extends Component {
+
+  get clips() {
+    if(!this.args.dummies) {
+      return this.args.clips
+    }
+
+    const clips = [];
+    for(const [index, clip] of this.args.clips.toArray().entries()) {
+      if(Math.random() < 1/(2 + index)){
+        clips.push("dummy");
+      }
+      clips.push(clip);
+    }
+
+    return clips;
+  }
+
+}
