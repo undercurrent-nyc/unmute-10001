e4f729874befbf8e5fb231a792cf7223a1a79848
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-08-03 21:24:10 -0400

    feat: Transition to exhibit/index from splash_page

--- app/components/splash-page.js
+++ app/components/splash-page.js
@@ -1,4 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from "@ember/object";
+import { inject as service } from '@ember/service';
 
 export default class SplashPageComponent extends Component {
+  @service router;
+
+  @action 
+  transition() {
+    this.router.transitionTo("exhibit.index");
+  }
+
 }
