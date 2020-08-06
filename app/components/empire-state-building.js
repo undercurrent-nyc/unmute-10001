0270414ee5b76906c228c294321dbdac8d5487cb
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-08-06 09:59:38 -0400

    feat: Speed up ESB animation and add enter button.

--- app/components/empire-state-building.js
+++ app/components/empire-state-building.js
@@ -5,6 +5,16 @@ import * as d3 from "d3";
 export default class EmpireStateBuildingComponent extends Component {
 
   fadeInTetrominoes() {
+    d3.select(".enter-button-background")
+      .style("fill", "black")
+      .on("mouseover", function() {
+        return d3.select(this)
+          .style("fill", "#33ff5E");
+      })
+      .on("mouseout", function() {
+        return d3.select(this)
+          .style("fill", "black");
+      });
     d3.select(".empire_state_tetrominoes")
       .selectAll("g")
       .style("opacity", 0.0);
@@ -14,7 +24,7 @@ export default class EmpireStateBuildingComponent extends Component {
         let opacity = d3.randomNormal(75, 10)()/100;
         d3.select(`.tetromino_${tetrominoIndices.pop()}`)
           .transition()
-            .duration(1000)
+            .duration(500)
             .ease(d3.easeLinear)
             .style("opacity", () => {
               if(opacity >= 1) {
@@ -27,13 +37,13 @@ export default class EmpireStateBuildingComponent extends Component {
         d3.select(".empire_state_tetrominoes")
           .selectAll("g")
           .transition()
-            .duration(500)
+            .duration(250)
             .ease(d3.easeLinear)
             .style("opacity", 1);
         setTimeout(() => {
           d3.selectAll(".skyline")
             .transition()
-              .duration(750)
+              .duration(250)
               .ease(d3.easeLinear)
               .style("opacity", 1);
         }, 500);
