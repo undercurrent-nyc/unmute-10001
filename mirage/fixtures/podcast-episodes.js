362cfb0f5b4e021937709d769b4e9c904562d387
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-08-22 18:31:13 -0400

    Add second podcast ep.

--- mirage/fixtures/podcast-episodes.js
+++ mirage/fixtures/podcast-episodes.js
@@ -1,5 +1,16 @@
 export default [
   {
+    id: "2",
+    sound: "https://chtbl.com/track/9474E/https://dts.podtrac.com/redirect.mp3/static1.squarespace.com/static/55199391e4b0f81fe422a536/t/5f418b0a0502cb348f9f9cb5/1598131014433/e12_mixdown_better_cover.mp3",
+    duration: "14:52",
+    postDate: "08/22",
+    number: 2,
+    url: "http://feedproxy.google.com/~r/undercurrent70/podcast/~3/Yv0OmOAwpnc/icebreaking-at-10001",
+    imageURL: "https://images.squarespace-cdn.com/content/v1/55199391e4b0f81fe422a536/1598130852391-72K8NBFSDF9GYO5CPPKO/ke17ZwdGBToddI8pDm48kKDa_N8M4hKxwB8FBtOrHTpZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFEw-wS7oHV2wLHiA9JhJhmzbdyMdfvUQXL4bUfZ_vUbhur-lC0WofN0YB1wFg-ZW0/Screen+Shot+2020-08-22+at+5.13.00+PM.png?format=1500w",
+    title: "Icebreaking at “10001”",
+    streamInfo: "Moacir introduces the six teams making up the collaborative virtual project “10001,” talks about how the various artists have a varying sense of belonging both to New York City and to their sponsor/home countries, and wonders about how they’ll collaborate over the next few weeks to try and imagine a post-pandemic New York City.",
+  },
+  {
     id: "1",
     sound: "https://chtbl.com/track/9474E/https://dts.podtrac.com/redirect.mp3/static1.squarespace.com/static/55199391e4b0f81fe422a536/t/5f2b227ab4970b0b1f4765b6/1596662475239/e-11_mixdown.mp3",
     duration: "16:29",
