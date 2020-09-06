d85cac5deab7b4901ccd89a06392ce5028e6423a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-09-05 20:14:36 -0400

    Create consultants page.

--- app/routes/exhibit/consultants.js
+++ app/routes/exhibit/consultants.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class ExhibitConsultantsRoute extends Route {
+  model() {
+    return this.store.findAll("consultant", {include: "teams,teams.artists,teams.artists.country,teams.clips" });
+  }
 }
